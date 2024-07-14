cae55e8332fa0115d3de3080e43bf20498cbb4da
shubham <shubhamkr5051@gmail.com>
2024-07-14 15:00:01 +0530

    fit Top-restro features

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,16 +1,35 @@
 import RestroCard from "./RestroCard";
 import restroList from "../utils/dummyData";
+import { useState } from "react";
 
-const BodyComponent =()=>{
-    return (
-     <div className="restro-list">
-      {
-        restroList.map((restro) => {        
-          return <RestroCard restaurants={restro} key={restro.info.id}/>
-        })
-      }
+const BodyComponent = () => {
+    //local state variable using react hooks :- useState
+ let[restraurantList, setrestraurantList]=useState(restroList);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filterBtn"
+          type="button"
+          onClick={() => {
+            newList = restraurantList.filter(
+              (restro) => restro.info.avgRating >= 4.3
+            );
+            setrestraurantList(newList);
+          }
+        }   
+        >
+          top-rated restro
+        </button>
+      </div>
+      <div className="restro-list">
+        {restraurantList.map((restro) => {
+          return <RestroCard restaurants={restro} key={restro.info.id} />;
+        })}
+      </div>
     </div>
-    )
-  }
+  );
+};
 
-  export default BodyComponent;
+export default BodyComponent;
