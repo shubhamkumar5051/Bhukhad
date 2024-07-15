27db5127e869b9f7eb8bf0ee41d0e2aed1bd1ee0
shubham <shubhamkr5051@gmail.com>
2024-07-15 17:11:18 +0530

    fit swiggy API for dynamic card

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,10 +1,22 @@
 import RestroCard from "./RestroCard";
-import restroList from "../utils/dummyData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const BodyComponent = () => {
     //local state variable using react hooks :- useState
- let[restraurantList, setrestraurantList]=useState(restroList);
+ let[restraurantList, setrestraurantList]=useState([]);
+
+
+//  json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+ useEffect( () => {
+  fetchData();
+ }, []);
+
+ const fetchData = async () =>{
+  const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.59080&lng=85.13480&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  const json = await data.json();
+  const jsonData = json.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+  setrestraurantList(jsonData);
+ }
 
   return (
     <div className="body">
--- src/components/RestroCard.js
+++ src/components/RestroCard.js
@@ -1,13 +1,15 @@
 import { cdn_img } from "../utils/link";
 
 const RestroCard=(props)=>{
-  const{cloudinaryImageId,name, cuisines, avgRating} = props.restaurants?.info;
+  const{cloudinaryImageId,name, cuisines, avgRating, costForTwo,sla} = props.restaurants?.info;
   return(
     <div className="card">
      <img src = {cdn_img + cloudinaryImageId} alt="image"></img>
      <h2>{name}</h2>
      <h3>{cuisines.join(", ")}</h3>
      <h4>{avgRating}</h4>
+     <h4>{costForTwo}</h4>
+     <h4>{sla.slaString}</h4>
     </div>
   )
 }
