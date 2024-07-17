9af2ddcb4b5d8b366bc4295eaa7f996e7eb874a3
shubham <shubhamkr5051@gmail.com>
2024-07-17 13:51:37 +0530

    Bug resolved - Search

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -3,62 +3,82 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const BodyComponent = () => {
-    //local state variable using react hooks :- useState
- const[restraurantList, setrestraurantList]=useState([]);
- const[searchTxt, setsearchTxt] = useState("");
+  //console.log("body re-render"); // for checking re-render cycle
 
-//  json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
- useEffect( () => {
-  fetchData();
- }, []);
- 
+  //local state variable using react hooks :- useState
+  const [restraurantList, setrestraurantList] = useState([]);
+  const [filterRestro, setfilterRestro] = useState([]);
+  const [searchTxt, setsearchTxt] = useState("");
 
- //Fetch api GET request using async/await:
- const fetchData = async () =>{
-  try{
-  const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.59080&lng=85.13480&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-  const json = await data.json();
-  const jsonData = json.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-  setrestraurantList(jsonData);
-  } catch (error)
-  {
-    console.log(error);
-  }
- }
+  //  json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+  useEffect(() => {
+    fetchData();
+  }, []);
 
- if (restraurantList.length === 0) {
-   return <Shimmer/>
- }
+  //Fetch api GET request using async/await:
+  const fetchData = async () => {
+    try {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.59080&lng=85.13480&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const json = await data.json();
+      const jsonData =
+        json.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants;
+      setrestraurantList(jsonData);
+      setfilterRestro(jsonData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  //conditional rendering
+  //  if (restraurantList.length === 0) {
+  //    return <Shimmer/>
+  //  }
 
-  return (
+  return restraurantList.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <div className="search">
-          <input type="text" className="search-box" value={searchTxt} onChange={(e)=>{setsearchTxt(e.target.value)}}/>
-          <button type="button" onClick={()=>{
-            newList1 = restraurantList.filter(
-              (restro) => restro.info.name.toLowerCase() == searchTxt.toLowerCase()
-            );
-            setrestraurantList(newList1);
-          }}>Search</button>
+          <input
+            type="text"
+            className="search-box"
+            value={searchTxt}
+            onChange={(e) => {
+              setsearchTxt(e.target.value);
+              //console.log(e);  //whenever local state var is changed the whole component get re-render. for every single button press. whole comp get painted again
+            }}
+          />
+          <button
+            type="button"
+            onClick={() => {
+              newList1 = restraurantList.filter((restro) =>
+                restro.info.name.toLowerCase().includes(searchTxt.toLowerCase())
+              );
+              setfilterRestro(newList1);
+            }}
+          >
+            Search
+          </button>
         </div>
         <button
           className="filterBtn"
           type="button"
           onClick={() => {
             newList = restraurantList.filter(
-              (restro) => restro.info.avgRating >= 4.3
+              (restro) => restro.info.avgRating >= 4.1
             );
-            setrestraurantList(newList);
-          }
-        }   
+            setfilterRestro(newList);
+          }}
         >
           top-rated restro
         </button>
       </div>
       <div className="restro-list">
-        {restraurantList.map((restro) => {
+        {filterRestro.map((restro) => {
           return <RestroCard restaurants={restro} key={restro.info.id} />;
         })}
       </div>
@@ -110,7 +130,6 @@ export default BodyComponent;
 //       console.log(error);
 //     }
 //   };
-  
 
 //   const handleScroll = () => {
 //     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
