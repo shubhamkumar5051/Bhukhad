69228d29b11df6e31a074eb377d0144c02e41797
shubham <shubhamkr5051@gmail.com>
2024-07-23 01:27:36 +0530

    Exploring Class based component in About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ const About = () => {
     <div>
         <h1>About US</h1>
         <h2>welcome to swiggy</h2>
-        <User name={"shubham from function"}/>
-        <UserClass name={"shubhamn from class"}/>
+    
+        <UserClass name={"shubhamn from CBC"} location={"himachal pradesh"}/>
          {/* here i will show the inforamtion about the team members using class based component in react */}
     </div>
   )
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,11 +5,34 @@ class UserClass extends React.Component{
     constructor(props)
     {
         super(props);
+
+        // making state variable
+
+        this.state= {
+            count : 0,
+            count1 : 1
+        }
     }
-    render(){
+    render()
+    {
+        const {name, location} = this.props;
+        const {count, count1} = this.state;
         return(
             <div>
-                <h1> name : {this.props.name}</h1>
+                <h1> name : {name}</h1>
+                <h2>{location}</h2>
+                <h3>
+                    Count : {this.state.count}  
+                    <button onClick={
+                        () =>{
+                           this.setState({
+                             count:count+1,
+                           })
+                        }
+                    }>
+                        click me
+                    </button>
+                </h3>
             </div>
         )
     }
