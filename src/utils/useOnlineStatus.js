01afca50ba49accd9a542f9e4fca5967115d0ce8
shubham <shubhamkr5051@gmail.com>
2024-07-24 22:02:10 +0530

    refactor custom hook - useOnlineStatus

--- src/utils/useOnlineStatus.js
+++ src/utils/useOnlineStatus.js
@@ -4,16 +4,20 @@ const useOnlineStatus=()=>{
     const[status, setStatus] = useState(true);
 
     useEffect(()=>{
+        checkOnline();
+    },[]);
+
+    const checkOnline=()=>{
         window.addEventListener("online", () => {
             setStatus(true);
         });
         window.addEventListener("offline", () => {
             setStatus(false);
         });
-    },[]);
+    }
 
     return status;
-};
+}
 
 export default useOnlineStatus;
 
